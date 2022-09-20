96a1a6c02623716f8a78248ef0acbd592e221efe
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-09-20 13:19:45 +0200

    powervers1

--- src/powerset.py
+++ src/powerset.py
@@ -7,6 +7,20 @@ def power(x: list[Any]) -> list[list[Any]]:
     """
     Compute the power-set of x.
 
-    Returns the power-set of x as a list of lists.
-    """
-    return []  # FIXME: you need to add some code here.
+ 
+
+#     Returns the power-set of x as a list of lists.
+#     """
+    power_set = []
+
+    for i in range(2**len(x)):
+        bits = format(i,"b").zfill(len(x))
+
+        subset = [x[j] for j in range(len(bits)) if bits[j] == '1']
+
+        power_set.append(subset)
+    
+    return power_set
+
+
+# print(power(['a','b','c']))
